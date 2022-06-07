10ab0ffd34e5247631affa83c7613141bdfa695a
Hannah Slater <hannahslater@MacBook-Pro.home>
2022-06-07 18:23:26 +0100

    full solution

--- RomanNumerals.java
+++ RomanNumerals.java
@@ -3,15 +3,44 @@ class RomanNumerals{
         //find length of string
         int length = s.length();
         int number = 0;
+        int minus = 0;
         for (int i = 0; i < length; i++){
-            int num = numeral(s.get(i));
-            
+            int num = numeral(s.charAt(i));
+            if (minus != 0){
+                num -= minus;
+                minus = 0;
+            }
+            if (i != length - 1){
+                if (num == 1){
+                    int nextNum = numeral(s.charAt(i+1));
+                    if (nextNum == 5 || nextNum == 10){
+                        minus = 1;
+                    }
+                    num = 0;
+                }
+                if (num == 10){
+                    int nextNum = numeral(s.charAt(i+1));
+                    if (nextNum == 50 || nextNum == 100){
+                        minus = 10;
+                    }
+                    num = 0;
+                }
+                if (num == 100){
+                    int nextNum = numeral(s.charAt(i+1));
+                    if (nextNum == 500 || nextNum == 1000){
+                        minus = 100;
+                    }
+                    num = 0;
+                }
+            }
+            number += num;
         }
         //for first letter, if I X or C check if V/X, L/C or D/M
         //
+        return number;
     }
     
-    public int numerals(char c){
+    public static int numeral(char c){
         int num = 0;
         switch(c){
             case 'I':
@@ -38,10 +67,9 @@ class RomanNumerals{
             default:
                 break;
         }
-
         return num;
     }
     public static void main(String[] args) {
-        System.out.println(romanToInt("III"));
+        System.out.println(romanToInt("MCMXCIV"));
     }
 }
